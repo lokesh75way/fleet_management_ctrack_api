1ebdcf53616dd48125e376725d5af851033d1493
Lokesh Bansal <lokesh@75way.com>
2024-04-04 17:18:13 +0530

    get alert by id integrated

--- app/controllers/Alert.ts
+++ app/controllers/Alert.ts
@@ -3,99 +3,134 @@ import Alert from "../schema/Alerts";
 import { createResponse } from "../helper/response";
 import createHttpError from "http-errors";
 
+export const addAlert = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const payload = req.body;
+    console.log(payload);
 
-export const addAlert = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const payload = req.body;
-        console.log(payload)
-
-        const checkIfExist = await Alert.findOne({ alertName: payload.alertName });
-        console.log(payload)
-        if(checkIfExist) {
-            throw createHttpError(400, {
-                message: `Alert already exist with this name!`,
-            });
-        }
-        const createAlert = await new Alert(payload).save();
-
-        res.send(
-            createResponse(createAlert, "Alert created successfully!")
-        );
-    } catch (error) {
-        throw createHttpError(400, {
-            message: error ?? "An error occurred.",
-            data: { user: null },
-        });
+    const checkIfExist = await Alert.findOne({ alertName: payload.alertName });
+    console.log(payload);
+    if (checkIfExist) {
+      throw createHttpError(400, {
+        message: `Alert already exist with this name!`,
+      });
     }
-} 
+    const createAlert = await new Alert(payload).save();
 
-export const updateAlert = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const payload = req.body;
-        const condition = { _id: req.params.id };
-        const options = { new: true };
+    res.send(createResponse(createAlert, "Alert created successfully!"));
+  } catch (error) {
+    throw createHttpError(400, {
+      message: error ?? "An error occurred.",
+      data: { user: null },
+    });
+  }
+};
 
-        const updateData = await Alert.findOneAndUpdate( condition, payload, options );
+export const updateAlert = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const payload = req.body;
+    const condition = { _id: req.params.id };
+    const options = { new: true };
 
-        if (!updateData) {
-            throw createHttpError(400, {
-                message: `Failed to update Alert!`,
-                data: { user: null },
-            });
-        }
-        res.send(
-            createResponse(updateData, `Alert updated successfully!`)
-        );
-    } catch (error) {
-        throw createHttpError(400, {
-            message: error,
-            data: { user: null },
-        })
+    const updateData = await Alert.findOneAndUpdate(
+      condition,
+      payload,
+      options
+    );
+
+    if (!updateData) {
+      throw createHttpError(400, {
+        message: `Failed to update Alert!`,
+        data: { user: null },
+      });
     }
-}
+    res.send(createResponse(updateData, `Alert updated successfully!`));
+  } catch (error) {
+    throw createHttpError(400, {
+      message: error,
+      data: { user: null },
+    });
+  }
+};
 
-export const deleteAlert = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const condition = { _id: req.params.id };
+export const deleteAlert = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const condition = { _id: req.params.id };
 
-        const updateData = await Alert.findOneAndUpdate( condition, { isDeleted: true } );
-        if (!updateData) {
-            throw createHttpError(400, {
-                message: `Failed to delete Alert!`,
-                data: { user: null },
-            });
-        }
-
-        res.send(
-            createResponse({}, `Alert deleted successfully!`)
-        );
-
-    } catch (error) {
-        throw createHttpError(400, {
-            message: error ?? "An error occurred.",
-            data: { user: null },
-        })
+    const updateData = await Alert.findOneAndUpdate(condition, {
+      isDeleted: true,
+    });
+    if (!updateData) {
+      throw createHttpError(400, {
+        message: `Failed to delete Alert!`,
+        data: { user: null },
+      });
     }
-}
 
-export const getAllAlerts = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const condition = {
-            isDeleted: false,
-        };
-        const data = await Alert.find(condition).sort({
-            createdAt: -1
-        });
-        const count = await Alert.count(condition);
+    res.send(createResponse({}, `Alert deleted successfully!`));
+  } catch (error) {
+    throw createHttpError(400, {
+      message: error ?? "An error occurred.",
+      data: { user: null },
+    });
+  }
+};
 
-        res.send(
-            createResponse({data, count}, `Alert found successfully!`)
-        );
+export const getAllAlerts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const condition = {
+      isDeleted: false,
+    };
+    const data = await Alert.find(condition).sort({
+      createdAt: -1,
+    });
+    const count = await Alert.count(condition);
 
-    } catch (error: any) {
-        throw createHttpError(400, {
-            message: error?.message ?? "An error occurred.",
-            data: { user: null },
-        })
-    }
-}
+    res.send(createResponse({ data, count }, `Alert found successfully!`));
+  } catch (error: any) {
+    throw createHttpError(400, {
+      message: error?.message ?? "An error occurred.",
+      data: { user: null },
+    });
+  }
+};
+
+export const getAlertById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params.id;
+    const condition = {
+      isDeleted: false,
+      _id: id,
+    };
+    const data = await Alert.findOne(condition).sort({
+      createdAt: -1,
+    });
+
+    res.send(createResponse({ data }, `Alert found successfully!`));
+  } catch (error: any) {
+    throw createHttpError(400, {
+      message: error?.message ?? "An error occurred.",
+      data: { user: null },
+    });
+  }
+};
--- app/routes/alert.ts
+++ app/routes/alert.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { catchError, validate } from "../middleware/validation";
-import { addAlert, getAllAlerts, deleteAlert, updateAlert } from "../controllers/Alert";
+import { addAlert, getAllAlerts, deleteAlert, updateAlert, getAlertById } from "../controllers/Alert";
 import asyncHandler from "express-async-handler";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post("/", validate("alert:add"), catchError, asyncHandler(addAlert));
 router.put("/:id", validate("alert:update"), catchError, asyncHandler(updateAlert));
 router.delete("/:id", asyncHandler(deleteAlert));
 router.get("/", asyncHandler(getAllAlerts));
+router.get("/:id",asyncHandler(getAlertById))
 
 export default router;
