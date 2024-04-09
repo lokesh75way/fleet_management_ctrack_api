6f1e31c2a93eac186e7f42cecc71f1649e532698
Lokesh Bansal <lokesh@75way.com>
2024-04-09 18:13:15 +0530

    bug fix

--- app/schema/Alerts.ts
+++ app/schema/Alerts.ts
@@ -1,42 +1,42 @@
 import mongoose, { type Types } from "mongoose";
 import { type BaseSchema } from "./index";
-import { IBranch } from "./CompanyBranch";
+import CompanyBranch, { IBranch } from "./CompanyBranch";
 
 const Schema = mongoose.Schema;
 
 export enum BasedOn {
-    VEHICLE = "VEHICLE",
-    VEHICLE_GROUP = "VEHICLE_GROUP",
-    VEHICLE_TYPE = "VEHICLE_TYPE",
+  VEHICLE = "VEHICLE",
+  VEHICLE_GROUP = "VEHICLE_GROUP",
+  VEHICLE_TYPE = "VEHICLE_TYPE",
 }
 
 export enum AlertValue {
-    START = "START",
-    CANCEL = "CANCEL",
-    BOTH = "BOTH",
+  START = "START",
+  CANCEL = "CANCEL",
+  BOTH = "BOTH",
 }
 
 export enum ValidDays {
-    EVERYDAY = "EVERYDAY",
-    CUSTOM = "CUSTOM",
+  EVERYDAY = "EVERYDAY",
+  CUSTOM = "CUSTOM",
 }
 
 export enum Severity {
-    LOW = 'LOW',
-    MEDIUM = 'MEDIUM',
-    HIGH = 'HIGH',
+  LOW = "LOW",
+  MEDIUM = "MEDIUM",
+  HIGH = "HIGH",
 }
 
 export enum AlertTypes {
-    EMERG_911 = "911",
-    CRASH_DETECTION = "Crash Detection",
-    BLE_BATTERY_SENSOR1 = "BLE Battery Sensor1",
-    BLE_BATTERY_SENSOR2 = "BLE Battery Sensor2",
-    BLE_BATTERY_SENSOR3 = "BLE Battery Sensor3",
+  EMERG_911 = "911",
+  CRASH_DETECTION = "Crash Detection",
+  BLE_BATTERY_SENSOR1 = "BLE Battery Sensor1",
+  BLE_BATTERY_SENSOR2 = "BLE Battery Sensor2",
+  BLE_BATTERY_SENSOR3 = "BLE Battery Sensor3",
 }
 
 export interface IAlert extends BaseSchema {
-  branchId: Types.ObjectId | IBranch;
+  branch: Types.ObjectId | IBranch;
   basedOn: BasedOn;
   object: string;
   objectGroup: string;
@@ -47,46 +47,46 @@ export interface IAlert extends BaseSchema {
   validFrom: Date;
   validTo: Date;
   action: string;
-  isDeleted : boolean;
+  isDeleted: boolean;
   severity: Severity;
 }
 
 const AlertSchema = new Schema<IAlert>(
   {
-    branchId: {
-        type: mongoose.Types.ObjectId,
-        ref: "company-branch"
+    branch: {
+      type: mongoose.Types.ObjectId,
+      ref: CompanyBranch.modelName,
     },
-    basedOn: { 
-        type: String,
-        enum: Object.values(BasedOn),
+    basedOn: {
+      type: String,
+      enum: Object.values(BasedOn),
     },
-    object: { 
-        type: String
+    object: {
+      type: String,
     },
-    objectGroup: { 
-        type: String
+    objectGroup: {
+      type: String,
     },
-    alertName: { 
-        type: String
+    alertName: {
+      type: String,
     },
-    isDeleted : {
-        type : Boolean,
-        default : false
+    isDeleted: {
+      type: Boolean,
+      default: false,
     },
-    alertType: { 
-        type: String,
-        enum: Object.values(AlertTypes),
+    alertType: {
+      type: String,
+      enum: Object.values(AlertTypes),
     },
     value: { type: String, enum: Object.values(AlertValue) },
     validDays: { type: String, enum: Object.values(ValidDays) },
     validFrom: { type: Date },
     validTo: { type: Date },
     action: {
-        SMS: { type: Boolean },
-        Email: { type: Boolean },
-        Notification: { type: Boolean }
-      },
+      SMS: { type: Boolean },
+      Email: { type: Boolean },
+      Notification: { type: Boolean },
+    },
     severity: { type: String, enum: Object.values(Severity) },
   },
   { timestamps: true }
