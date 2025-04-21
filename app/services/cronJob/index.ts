da9466112c63ad135e2a35d0baa71e8653f7463e
Lokesh Bansal <lokesh@75way.com>
2025-04-21 19:01:55 +0530

    minor changes

--- app/services/cronJob/index.ts
+++ app/services/cronJob/index.ts
@@ -4,12 +4,12 @@ import {UnassignedVehicleData, trackingData } from './cronJob';
 export const initCronJob = async()=>{
   cron.schedule('*/3 * * * *', async() => {
     // console.log("Cron job running")
-    // await trackingData();
+    await trackingData();
   });
 
   // At 12 AM Everyday
   cron.schedule('0 0 * * *', async() => {
     // console.log("Cron job for get unassigned vehicle")
-    // await UnassignedVehicleData();
+    await UnassignedVehicleData();
   });
 }
